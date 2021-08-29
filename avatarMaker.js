03119eb1557460909f94c52824a3e609c6838677
Alice Quintanilla <alice.quintanilla98@gmail.com>
2021-08-29 00:47:36 -0500

    better bodies, eyes, sameCategory()
    added final (actually good!) images for bodies and eyes.
    also changed the means of determining whether two items were in the same category from using the category tag (didnt actually work) to comparing the first 4 characters of the element's id (same as file name)

--- avatarMaker.js
+++ avatarMaker.js
@@ -1,14 +1,9 @@
-let defaultColor = "#A9A9A9" //dark grey
+let avatar = [] //array of images for tracking which images are currently displayed
 
-let avatar = [] //array of images. tracks which images are currently displayed
-//to start with defaultBody, defaultFace
-
-//called when user clicks on a button for a clothing-item
-//id: id of the image corresponding to the buttom they clicked.
+//if the clothing* selection is visible, change out of it.
+//if the clothing* selection is not visible, change into it.
 function toggleVisibility(id){
-  // console.log(avatar);
   let element = document.getElementById(id)
-  // console.log(element);
   if( avatar.includes(element) ){
     changeOutOf(element)
   } else {
@@ -16,10 +11,13 @@ function toggleVisibility(id){
   }
 }
 
+//makes newElement visible, and makes invisible all elements that share
+//the same "category" tag.
 function changeInto(newElement){
+  console.log(newElement.class);
   newElement.style.visibility = 'visible'
   avatar.forEach((oldElement, i) => {
-    if(oldElement.category == newElement.category) changeOutOf(oldElement)
+    if(sameCategory(oldElement,newElement)) changeOutOf(oldElement)
     })
   avatar.push(newElement)
 }
@@ -27,4 +25,10 @@ function changeInto(newElement){
 function changeOutOf(oldElement){
   avatar.splice(avatar.indexOf(oldElement), 1)
   oldElement.style.visibility = 'hidden'
+}
+
+//a, b: img elements representing style-options for the avatar
+//this works because the id of every mouth-image starts with the word "mouth", etc
+function sameCategory(a, b){
+  return a.id.substring(0,3) == b.id.substring(0,3)
 }
