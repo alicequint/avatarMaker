777c0f6caaca002549a25d14ee1ddb90c6f35f75
Alice Quintanilla <alice.quintanilla98@gmail.com>
2021-08-31 02:31:39 -0500

    frame
    avatar now appears in a pleasant frame.
    removed unused code + images.
    added (unfinished) labels, download button, random button, misc.

--- avatarMaker.js
+++ avatarMaker.js
@@ -1,5 +1,8 @@
 let avatar = [] //array of images for tracking which images are currently displayed
 
+// let avatarImages = [[]] //TODO: surely there is an easy way to populate this, right?
+
+
 //if the clothing* selection is visible, change out of it.
 //if the clothing* selection is not visible, change into it.
 function toggleVisibility(id){
@@ -31,3 +34,18 @@ function changeOutOf(oldElement){
 function sameCategory(a, b){
   return a.id.substring(0,3) == b.id.substring(0,3)
 }
+
+// //randomly selects and changes into one style from each available category
+// //TODO: avatarImages is not populated yet
+// function randomizeAvatar() {
+//   avatarImages.forEach((category, i) => {
+//     let newElement = category[Math.floor(Math.random() * 5)]
+//     changeInto(newElement)
+//   });
+// }
+//
+// function createDownloadableImage() {
+//   console.log(avatar);
+//   // TODO: lay all the images in avatar over each other, creating one image.
+//   return null;
+// }
