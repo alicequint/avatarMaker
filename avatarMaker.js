4bb8e86186aec85501cc67e9e39b013f362adb40
Alice Quintanilla <alice.quintanilla98@gmail.com>
2021-09-01 03:22:57 -0500

    hair and random button
    added hairstyles
    random button now works

--- avatarMaker.js
+++ avatarMaker.js
@@ -1,6 +1,11 @@
 let avatar = [] //array of images for tracking which images are currently displayed
 
-// let avatarImages = [[]] //TODO: surely there is an easy way to populate this, right?
+let avatarImageIDs = [
+  ["body-angular","body-ghost","body-purple","body-soft","body-waving"],
+  ["eyes-diamond","eyes-excited","eyes-glasses","eyes-intense","eyes-modest"],
+  ["mouth-animal","mouth-chewy","mouth-jagged","mouth-smile","mouth-surprised"],
+  ["hair-crown","hair-curly","hair-long","hair-spiky","hair-sprout"],
+]
 
 
 //if the clothing* selection is visible, change out of it.
@@ -35,14 +40,18 @@ function sameCategory(a, b){
   return a.id.substring(0,3) == b.id.substring(0,3)
 }
 
-// //randomly selects and changes into one style from each available category
-// //TODO: avatarImages is not populated yet
-// function randomizeAvatar() {
-//   avatarImages.forEach((category, i) => {
-//     let newElement = category[Math.floor(Math.random() * 5)]
-//     changeInto(newElement)
-//   });
-// }
+//randomly selects and changes into one style from each available category
+//TODO: avatarImages is not populated yet
+function randomizeAvatar() {
+  console.log(avatar);
+  console.log(avatarImageIDs);
+  avatarImageIDs.forEach((category, i) => {
+    let randomID = category[Math.floor(Math.random() * 5)]
+    console.log(randomID);
+    let newElement = document.getElementById(randomID)
+    changeInto(newElement)
+  });
+}
 //
 // function createDownloadableImage() {
 //   console.log(avatar);
