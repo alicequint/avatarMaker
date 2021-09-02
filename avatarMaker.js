2c75cf60790593818fd6572e9e5125e2d688675d
Alice Quintanilla <alice.quintanilla98@gmail.com>
2021-09-02 18:49:08 -0500

    changed approach (using canvas now)
    functionality should be mostly the same.
    moved to rendering a canvas rather than showing/hiding images by editing display tags, because it will be easier to implement Download button now.

--- avatarMaker.js
+++ avatarMaker.js
@@ -1,5 +1,7 @@
-let avatar = [] //array of images for tracking which images are currently displayed
+//array of images for tracking user style selections
+let avatar = []
 
+//for use in randomizeAvatar()
 let avatarImageIDs = [
   ["body-angular","body-ghost","body-purple","body-soft","body-waving"],
   ["eyes-diamond","eyes-excited","eyes-glasses","eyes-intense","eyes-modest"],
@@ -7,9 +9,8 @@ let avatarImageIDs = [
   ["hair-crown","hair-curly","hair-long","hair-spiky","hair-sprout"],
 ]
 
-
-//if the clothing* selection is visible, change out of it.
-//if the clothing* selection is not visible, change into it.
+//if the style selection is visible, change out of it.
+//if the style selection is not visible, change into it.
 function toggleVisibility(id){
   let element = document.getElementById(id)
   if( avatar.includes(element) ){
@@ -17,44 +18,58 @@ function toggleVisibility(id){
   } else {
     changeInto(element)
   }
+  redrawAvatar()
 }
 
-//makes newElement visible, and makes invisible all elements that share
-//the same "category" tag.
+//updates canvas to be reflective of avatar[]
+function redrawAvatar(){
+  let context = document.getElementById('avatarCanvas').getContext('2d')
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+  //sort avatar so that the images get stacked correctly...
+  let sortedAvatar = [null,null,null,null]
+  avatar.forEach((element) => {
+    if(element.className == 'body') sortedAvatar[0]=element
+    else if(element.className =='hair') sortedAvatar[1]=element
+    else if(element.className == 'mouth') sortedAvatar[2]=element
+    else if(element.className == 'eyes') sortedAvatar[3]=element
+  });
+  sortedAvatar.forEach((element) => {
+    if(element != null)
+      context.drawImage(element,0,0)
+  });
+}
+
+//adds newElement to avatar, removes all other elements that share a category with it
 function changeInto(newElement){
-  newElement.style.visibility = 'visible'
   avatar.forEach((oldElement, i) => {
-    if(sameCategory(oldElement,newElement)) changeOutOf(oldElement)
-    })
+    if(oldElement.className == newElement.className)
+      changeOutOf(oldElement)
+  })
   avatar.push(newElement)
-}
-
-function changeOutOf(oldElement){
-  avatar.splice(avatar.indexOf(oldElement), 1)
-  oldElement.style.visibility = 'hidden'
 }
 
 //a, b: img elements representing style-options for the avatar
 //this works because the id of every mouth-image starts with the word "mouth", etc
-function sameCategory(a, b){
-  return a.id.substring(0,3) == b.id.substring(0,3)
+// function sameCategory(imgElement1, imageElement2){
+//   return imgElement1.id.substring(0,3) == imageElement2.id.substring(0,3)
+// }
+
+function changeOutOf(oldElement){
+  avatar.splice(avatar.indexOf(oldElement), 1)
 }
 
 //randomly selects and changes into one style from each available category
 //TODO: avatarImages is not populated yet
 function randomizeAvatar() {
-  console.log(avatar);
-  console.log(avatarImageIDs);
   avatarImageIDs.forEach((category, i) => {
     let randomID = category[Math.floor(Math.random() * 5)]
-    console.log(randomID);
     let newElement = document.getElementById(randomID)
     changeInto(newElement)
+    redrawAvatar()
   });
 }
-//
-// function createDownloadableImage() {
-//   console.log(avatar);
-//   // TODO: lay all the images in avatar over each other, creating one image.
-//   return null;
-// }
+
+function createDownloadableImage() {
+  console.log(avatar);
+  // TODO: canvas to image.
+}
